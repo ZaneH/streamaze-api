10e2fb639827707d19ff551fb2b1346dfe8e2730
Zane Helton <zane99@me.com>
2023-03-02 20:32:22 -0500

    UI improvements

--- lib/streamaze_web/controllers/page_controller.ex
+++ lib/streamaze_web/controllers/page_controller.ex
@@ -4,7 +4,7 @@ defmodule StreamazeWeb.PageController do
   alias Streamaze.Streams
 
   def index(conn, _params) do
-    live_list = Streams.list_live_streams()
+    live_list = Streams.list_live_streams() |> Enum.uniq_by(& &1.streamer_id)
     render(conn, "index.html", live_list: live_list)
   end
 end
