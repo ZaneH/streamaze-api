b77a7477b3c695dd0c186e755bb27b4a5b5ea2b2
Zane Helton <zane99@me.com>
2023-04-03 15:01:42 -0400

    Fix TTS bug

--- lib/streamaze/tts.ex
+++ lib/streamaze/tts.ex
@@ -1,7 +1,7 @@
 defmodule Streamaze.TTS do
   def text_to_speech(text, voice_id, api_key) do
-    url = text_to_speech_url(voice_id, api_key)
-    headers = [{"Content-Type", "application/json"}]
+    url = text_to_speech_url(voice_id)
+    headers = [{"Content-Type", "application/json"}, {"xi-api-key", api_key}]
     {:ok, body} = Jason.encode(%{text: text})
 
     case HTTPoison.post(url, body, headers) do
@@ -19,9 +19,8 @@ defmodule Streamaze.TTS do
     HTTPoison.get!(url, headers) |> Map.get(:body) |> Jason.decode!() |> Map.get("voices")
   end
 
-  defp text_to_speech_url(voice_id, api_key) do
+  defp text_to_speech_url(voice_id) do
     URI.parse("https://api.elevenlabs.io/v1/text-to-speech/#{voice_id}")
-    |> URI.append_query("xi-api-key=#{api_key}")
     |> URI.to_string()
   end
 
