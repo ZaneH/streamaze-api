a859bd72de338eaaa31230a8739934da779fcc13
Zane Helton <zane99@me.com>
2023-04-13 13:45:56 -0400

    Fix TTL time

--- lib/streamaze_web/controllers/tts_controller.ex
+++ lib/streamaze_web/controllers/tts_controller.ex
@@ -58,8 +58,8 @@ defmodule StreamazeWeb.TTSController do
         :get,
         "elevenlabsaudio",
         file_name,
-        # 14 days
-        expires_in: 14 * 24 * 60 * 60
+        # 7 days (max)
+        expires_in: 7 * 24 * 60 * 60
       )
 
     s3_url
