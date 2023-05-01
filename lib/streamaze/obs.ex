f626b3b1b089be4d6401d8a8f0aee052ea7d0e7a
Zane Helton <zane99@me.com>
2023-05-01 02:36:49 -0400

    Update Livebond request logic

--- lib/streamaze/obs.ex
+++ lib/streamaze/obs.ex
@@ -1,20 +1,12 @@
 defmodule Streamaze.OBS do
   def switch_scene(streamer_key, scene) do
     case HTTPoison.put(create_request_url("/streamers/#{streamer_key}/scenes/#{scene}")) do
-      {:ok, %HTTPoison.Response{status_code: 200}} ->
+      {:ok, %HTTPoison.Response{status_code: 204}} ->
         :ok
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, "Scene not found"}
-
-      {:ok, %HTTPoison.Response{status_code: 500}} ->
-        {:error, "Internal server error"}
-
-      {:ok, %HTTPoison.Response{status_code: 502}} ->
-        {:error, "Unknown error"}
-
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
+      e ->
+        IO.inspect(e)
+        {:error, "Bad response code"}
     end
   end
 
@@ -22,105 +14,65 @@ defmodule Streamaze.OBS do
     qs = service_to_url_params(String.downcase(service))
     start_url = create_request_url("/streamers/#{streamer_key}/server/start?#{qs}")
 
-    case HTTPoison.post(start_url, {}) do
-      {:ok, %HTTPoison.Response{status_code: 200}} ->
+    case HTTPoison.post(start_url, "") do
+      {:ok, %HTTPoison.Response{status_code: 204}} ->
         :ok
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, "Streamer not found"}
-
-      {:ok, %HTTPoison.Response{status_code: 500}} ->
-        {:error, "Internal server error"}
-
-      {:ok, %HTTPoison.Response{status_code: 502}} ->
-        {:error, "Unknown error"}
-
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
+      e ->
+        IO.inspect(e)
+        {:error, "Bad response code"}
     end
   end
 
   def stop_server(streamer_key) do
     stop_url = create_request_url("/streamers/#{streamer_key}/server/stop")
 
-    case HTTPoison.post(stop_url, {}) do
-      {:ok, %HTTPoison.Response{status_code: 200}} ->
+    case HTTPoison.post(stop_url, "") do
+      {:ok, %HTTPoison.Response{status_code: 204}} ->
         :ok
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, "Streamer not found"}
-
-      {:ok, %HTTPoison.Response{status_code: 500}} ->
-        {:error, "Internal server error"}
-
-      {:ok, %HTTPoison.Response{status_code: 502}} ->
-        {:error, "Unknown error"}
-
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
+      e ->
+        IO.inspect(e)
+        {:error, "Bad response code"}
     end
   end
 
   def start_broadcast(streamer_key) do
     start_url = create_request_url("/streamers/#{streamer_key}/stream/start")
 
-    case HTTPoison.post(start_url, {}) do
-      {:ok, %HTTPoison.Response{status_code: 200}} ->
+    case HTTPoison.post(start_url, "") do
+      {:ok, %HTTPoison.Response{status_code: 204}} ->
         :ok
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, "Streamer not found"}
-
-      {:ok, %HTTPoison.Response{status_code: 500}} ->
-        {:error, "Internal server error"}
-
-      {:ok, %HTTPoison.Response{status_code: 502}} ->
-        {:error, "Unknown error"}
-
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
+      e ->
+        IO.inspect(e)
+        {:error, "Bad response code"}
     end
   end
 
   def stop_broadcast(streamer_key) do
     stop_url = create_request_url("/streamers/#{streamer_key}/stream/stop")
 
-    case HTTPoison.post(stop_url, {}) do
-      {:ok, %HTTPoison.Response{status_code: 200}} ->
+    case HTTPoison.post(stop_url, "") do
+      {:ok, %HTTPoison.Response{status_code: 204}} ->
         :ok
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, "Streamer not found"}
-
-      {:ok, %HTTPoison.Response{status_code: 500}} ->
-        {:error, "Internal server error"}
-
-      {:ok, %HTTPoison.Response{status_code: 502}} ->
-        {:error, "Unknown error"}
-
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
+      e ->
+        IO.inspect(e)
+        {:error, "Bad response code"}
     end
   end
 
   def stop_pi(streamer_key) do
     stop_url = create_request_url("/streamers/#{streamer_key}/device/shutdown")
 
-    case HTTPoison.post(stop_url, {}) do
-      {:ok, %HTTPoison.Response{status_code: 200}} ->
+    case HTTPoison.post(stop_url, "") do
+      {:ok, %HTTPoison.Response{status_code: 204}} ->
         :ok
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, "Streamer not found"}
-
-      {:ok, %HTTPoison.Response{status_code: 500}} ->
-        {:error, "Internal server error"}
-
-      {:ok, %HTTPoison.Response{status_code: 502}} ->
-        {:error, "Unknown error"}
-
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
+      e ->
+        IO.inspect(e)
+        {:error, "Bad response code"}
     end
   end
 
