a831fd5430ba1a09e6b2be98094eec1a4d5a1ad4
Zane Helton <zane99@me.com>
2023-07-26 08:41:37 -0400

    Remove viewers agent

--- lib/streamaze/connectivity/viewers.ex
+++ lib/streamaze/connectivity/viewers.ex
@@ -26,55 +26,6 @@ defmodule Streamaze.Connectivity.Viewers do
     end
   end
 
-  def fetch_kick_viewer_count(pid) do
-    try do
-      username =
-        Agent.get(pid, fn state ->
-          state["config"]["kick_channel_name"]
-        end)
-
-      response =
-        HTTPoison.get(
-          "https://hmmwhat.highstreaming.xyz/?url=https://kick.com/api/v2/channels/#{username}",
-          [],
-          hackney: [:insecure]
-        )
-
-      case response do
-        {:ok, %{body: body}} ->
-          case Jason.decode(body) do
-            {:ok, decoded} ->
-              livestream = Map.get(decoded, "livestream")
-
-              case livestream do
-                nil ->
-                  nil
-
-                livestream ->
-                  viewer_count = Map.get(livestream, "viewer_count")
-
-                  case viewer_count do
-                    nil ->
-                      nil
-
-                    viewer_count ->
-                      viewer_count
-                  end
-              end
-
-            {:error, _} ->
-              nil
-          end
-
-        {:error, _} ->
-          nil
-      end
-    rescue
-      e ->
-        IO.inspect(e, label: "Error fetching viewer count")
-    end
-  end
-
   # def fetch_youtube_viewer_count(pid) do
   #   Agent.get(pid, fn state ->
   #     config = state["config"]
