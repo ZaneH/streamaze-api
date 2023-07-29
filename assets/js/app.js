08d21059e62e55e4e8775846a25998370c035c4e
Zane Helton <zane99@me.com>
2023-07-28 22:45:49 -0400

    Layout new root for UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,8 @@
 //     import "some-package"
 //
 
+import "flowbite/dist/flowbite.phoenix.js";
+
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
