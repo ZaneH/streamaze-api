f5b98230de2a6e7956a560c7818cf463586b8bce
Zane Helton <zane99@me.com>
2023-08-05 12:02:56 -0400

    Subtle improvements to pagination

---
+++ lib/streamaze_web/live/components/conditional_link.ex
@@ -0,0 +1,16 @@
+defmodule ConditionalLink do
+  use Phoenix.Component
+  import Phoenix.HTML.Link
+
+  def render(assigns) do
+    ~H"""
+    <%= if @condition do %>
+      <%= link to: @to do %>
+        <%= render_slot(@inner_block) %>
+      <% end %>
+    <% else %>
+      <%= render_slot(@inner_block) %>
+    <% end %>
+    """
+  end
+end
